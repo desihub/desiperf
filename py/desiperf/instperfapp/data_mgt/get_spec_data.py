7b2f70686cb28025055c98760f04344b25f15f11
Parker <parfa30@gmail.com>
2020-09-18 15:54:27 -0700

    tab fix

--- py/desiperf/instperfapp/data_mgt/get_spec_data.py
+++ py/desiperf/instperfapp/data_mgt/get_spec_data.py
@@ -27,22 +27,22 @@ class SPECData():
         exp_df_new = exp_df[exp_cols]
         self.exp_df_new = self.exp_df_new.rename(columns={'id':'EXPID'})
         self.nights = np.unique(self.exp_df_new['night'])
-		self.dates = [int(d) for d in self.nights[np.isfinite(self.nights)]]
+        self.dates = [int(d) for d in self.nights[np.isfinite(self.nights)]]
 
         self.exp_df_base = self.exp_df_new[['EXPID','date_obs']]
 
 
     def get_spec_df(self):
-    	spec_cols = ['nir_camera_temp', 'nir_camera_humidity','red_camera_temp', 'red_camera_humidity', 'blue_camera_temp','blue_camera_humidity', 
-    		'bench_cryo_temp', 'bench_nir_temp','bench_coll_temp', 'ieb_temp', 'time_recorded', 'unit']
+        spec_cols = ['nir_camera_temp', 'nir_camera_humidity','red_camera_temp', 'red_camera_humidity', 'blue_camera_temp','blue_camera_humidity', 
+            'bench_cryo_temp', 'bench_nir_temp','bench_coll_temp', 'ieb_temp', 'time_recorded', 'unit']
 
-		spec_df = pd.read_sql_query(f"SELECT * FROM spectrographs_sensors WHERE time_recorded >= '{self.start_date}' AND time_recorded <'{self.end_date}'", self.conn)
-		spec_df_new = spec_df[spec_cols]
+        spec_df = pd.read_sql_query(f"SELECT * FROM spectrographs_sensors WHERE time_recorded >= '{self.start_date}' AND time_recorded <'{self.end_date}'", self.conn)
+        spec_df_new = spec_df[spec_cols]
 
-		dfs = []
-		for un in range(10):
-		    df = spec_df_new[spec_df_new.unit == un]
-		    cold = {}
+        dfs = []
+        for un in range(10):
+            df = spec_df_new[spec_df_new.unit == un]
+            cold = {}
             for col in df.columns:
                 new_col = col + '_' + str(i)
                 cold[col] = new_col
@@ -55,20 +55,20 @@ class SPECData():
             df = df.reset_index(drop=True)
             dfs.append(df)
 
-		self.spec_df_final = pd.concat(dfs, axis=1)
-		#spec_df.to_csv('spec_by_unit.csv')
+        self.spec_df_final = pd.concat(dfs, axis=1)
+        #spec_df.to_csv('spec_by_unit.csv')
 
-		spec_mean_df = self.exp_df_base.copy()
-		for attr in ['nir_camera_temp', 'nir_camera_humidity','red_camera_temp', 'red_camera_humidity', 'blue_camera_temp','blue_camera_humidity', 'bench_cryo_temp', 'bench_nir_temp','bench_coll_temp', 'ieb_temp']:
-		    x = []
-		    for i in range(10):
-		        df = new_dfs[i]
-		        x.append(df[attr+'_{}'.format(i)])
-		    spec_mean_df[attr+'_mean'] = np.mean(x, axis=0)
+        spec_mean_df = self.exp_df_base.copy()
+        for attr in ['nir_camera_temp', 'nir_camera_humidity','red_camera_temp', 'red_camera_humidity', 'blue_camera_temp','blue_camera_humidity', 'bench_cryo_temp', 'bench_nir_temp','bench_coll_temp', 'ieb_temp']:
+            x = []
+            for i in range(10):
+                df = new_dfs[i]
+                x.append(df[attr+'_{}'.format(i)])
+            spec_mean_df[attr+'_mean'] = np.mean(x, axis=0)
 
-		self.spec_mean_df_final = self.remove_repeats(selc_mean_df) del spec_mean_df['EXPID']
+        self.spec_mean_df_final = self.remove_repeats(selc_mean_df) del spec_mean_df['EXPID']
 
-	def get_gfa_df(self):
+    def get_gfa_df(self):
         gfa_cols = ['time_recorded','ccdtemp','hotpeltier','coldpeltier','filter','humid2','humid3','fpga','camerahumid','cameratemp','unit']
 
         gfa_df = pd.read_sql_query(f"SELECT * FROM gfa_telemetry WHERE time_recorded >= '{self.start_date}' AND time_recorded <'{self.end_date}'", self.conn)
@@ -96,7 +96,7 @@ class SPECData():
         self.gfa_df_final = pd.concat(dfs, axis=1)
 
     def get_shack_df(self):
-    	shack_cols = ['room_pressure','space_temp1', 'reheat_temp', 'space_humidity','time_recorded', 'heater_output', 'space_temp2', 'space_temp4','space_temp_avg', 'space_temp3', 'cooling_coil_temp','chilled_water_output']
+        shack_cols = ['room_pressure','space_temp1', 'reheat_temp', 'space_humidity','time_recorded', 'heater_output', 'space_temp2', 'space_temp4','space_temp_avg', 'space_temp3', 'cooling_coil_temp','chilled_water_output']
 
         shack_df = pd.read_sql_query(f"SELECT * FROM shack_wec WHERE time_recorded >= '{self.start_date}' AND time_recorded <'{self.end_date}'", self.conn)
         shack_df = shack_df[shack_cols]
@@ -109,102 +109,102 @@ class SPECData():
         self.shack_df_final = shack_df_new.reset_index(drop=True)
 
     def combine_specs(self, df_, cols_):
-	    dfs = []
-	    for un in range(10):
-	        cols = []
-	        for attr in cols_:
-	            cols.append(attr+'_{}'.format(un))
-	        df = df_[cols]
-	        df['SPECTRO'] = un
-	        new_cols = {}
-	        for col in cols_:
-	            new_cols[col+'_{}'.format(un)] = col
-	        df = df.rename(columns=new_cols)
-	        pd.concat([self.exp_df_base, df], axis = 1)
-	        dfs.append(df)
-	    
-	    df_final  = pd.concat(dfs)
-	    return df_final
+        dfs = []
+        for un in range(10):
+            cols = []
+            for attr in cols_:
+                cols.append(attr+'_{}'.format(un))
+            df = df_[cols]
+            df['SPECTRO'] = un
+            new_cols = {}
+            for col in cols_:
+                new_cols[col+'_{}'.format(un)] = col
+            df = df.rename(columns=new_cols)
+            pd.concat([self.exp_df_base, df], axis = 1)
+            dfs.append(df)
+        
+        df_final  = pd.concat(dfs)
+        return df_final
 
-	def get_qa_data(self, f):
-		per_amp_cols = ['NIGHT', 'EXPID', 'SPECTRO','CAM', 'AMP', 'READNOISE','BIAS', 'COSMICS_RATE']
+    def get_qa_data(self, f):
+        per_amp_cols = ['NIGHT', 'EXPID', 'SPECTRO','CAM', 'AMP', 'READNOISE','BIAS', 'COSMICS_RATE']
 
-	    hdulist = fits.open(f)
-	    hdu_names = [hdulist[i].name for i in range(len(hdulist))]
-	    if 'PER_AMP' in hdu_names:
-	        per_amp_df = Table(hdulist['PER_AMP'].data).to_pandas()
-	        final_df = per_amp_df[per_amp_cols]
-	    if 'PER_CAMERA' in hdu_names:
-	        per_cam_df = Table(hdulist['PER_CAMERA'].data).to_pandas()
-	        final_df = pd.merge(final_df, per_cam_df, on=['NIGHT','EXPID','SPECTRO','CAM'],how='left')
-	    if 'PER_CAMFIBER' in hdu_names:
-	        df = Table(hdulist['PER_CAMFIBER'].data).to_pandas()
-	        mean_values = []
-	        for cam in np.unique(df.CAM):
-	            ddf = df[df.CAM == cam]
-	            for spec in np.unique(df.SPECTRO):
-	                dd = ddf[ddf.SPECTRO == spec]
-	                vals = [cam, spec]
-	                for attr in ['INTEG_RAW_FLUX','MEDIAN_RAW_FLUX', 'MEDIAN_RAW_SNR']:
-	                    x = np.mean(dd[attr])
-	                    vals.append(x)
-	                mean_values.append(vals)
-	        per_camf_df = pd.DataFrame(np.vstack(mean_values), columns = ['CAM','SPECTRO','INTEG_RAW_FLUX','MEDIAN_RAW_FLUX', 'MEDIAN_RAW_SNR'])
-	        per_camf_df['SPECTRO'] = per_camf_df['SPECTRO'].astype('int64')
-	        final_df = pd.merge(final_df, per_camf_df, on = ['SPECTRO','CAM'], how='left')
+        hdulist = fits.open(f)
+        hdu_names = [hdulist[i].name for i in range(len(hdulist))]
+        if 'PER_AMP' in hdu_names:
+            per_amp_df = Table(hdulist['PER_AMP'].data).to_pandas()
+            final_df = per_amp_df[per_amp_cols]
+        if 'PER_CAMERA' in hdu_names:
+            per_cam_df = Table(hdulist['PER_CAMERA'].data).to_pandas()
+            final_df = pd.merge(final_df, per_cam_df, on=['NIGHT','EXPID','SPECTRO','CAM'],how='left')
+        if 'PER_CAMFIBER' in hdu_names:
+            df = Table(hdulist['PER_CAMFIBER'].data).to_pandas()
+            mean_values = []
+            for cam in np.unique(df.CAM):
+                ddf = df[df.CAM == cam]
+                for spec in np.unique(df.SPECTRO):
+                    dd = ddf[ddf.SPECTRO == spec]
+                    vals = [cam, spec]
+                    for attr in ['INTEG_RAW_FLUX','MEDIAN_RAW_FLUX', 'MEDIAN_RAW_SNR']:
+                        x = np.mean(dd[attr])
+                        vals.append(x)
+                    mean_values.append(vals)
+            per_camf_df = pd.DataFrame(np.vstack(mean_values), columns = ['CAM','SPECTRO','INTEG_RAW_FLUX','MEDIAN_RAW_FLUX', 'MEDIAN_RAW_SNR'])
+            per_camf_df['SPECTRO'] = per_camf_df['SPECTRO'].astype('int64')
+            final_df = pd.merge(final_df, per_camf_df, on = ['SPECTRO','CAM'], how='left')
 
-	    if 'PER_FIBER' in hdu_names:
-	        df = Table(hdulist['PER_FIBER'].data).to_pandas()
-	        mean_values  =[]
-	        for cam in ['B','R','Z']:
-	            for spec in np.unique(df.SPECTRO):
-	                ddf = df[df.SPECTRO == spec]
-	                vals = [cam, spec]
-	                for attr in ['FLUX_','SNR_','SPECFLUX_','THRU_']:
-	                    try:
-	                        x = np.mean(ddf[attr+cam])
-	                    except:
-	                        if cam == 'B':
-	                            try:
-	                                x = np.mean(ddf[attr+'G'])
-	                            except:
-	                                x = np.nan
-	                    vals.append(x)
-	                mean_values.append(vals)
-	        per_fib_df = pd.DataFrame(np.vstack(mean_values), columns = ['CAM','SPECTRO','FLUX','SNR','SPECFLUX','THRU'])
-	        per_fib_df['SPECTRO'] = per_fib_df['SPECTRO'].astype('int64')
-	        final_df = pd.merge(final_df, per_fib_df, on=['CAM','SPECTRO'], how='left')
-	    return final_df
+        if 'PER_FIBER' in hdu_names:
+            df = Table(hdulist['PER_FIBER'].data).to_pandas()
+            mean_values  =[]
+            for cam in ['B','R','Z']:
+                for spec in np.unique(df.SPECTRO):
+                    ddf = df[df.SPECTRO == spec]
+                    vals = [cam, spec]
+                    for attr in ['FLUX_','SNR_','SPECFLUX_','THRU_']:
+                        try:
+                            x = np.mean(ddf[attr+cam])
+                        except:
+                            if cam == 'B':
+                                try:
+                                    x = np.mean(ddf[attr+'G'])
+                                except:
+                                    x = np.nan
+                        vals.append(x)
+                    mean_values.append(vals)
+            per_fib_df = pd.DataFrame(np.vstack(mean_values), columns = ['CAM','SPECTRO','FLUX','SNR','SPECFLUX','THRU'])
+            per_fib_df['SPECTRO'] = per_fib_df['SPECTRO'].astype('int64')
+            final_df = pd.merge(final_df, per_fib_df, on=['CAM','SPECTRO'], how='left')
+        return final_df
 
-	def remove_repeats(self, df):
+    def remove_repeats(self, df):
         df.drop(['EXPID','date_obs'], axis=1, inplace=True)
         return df
 
     def get_qa_df(self):
-    	all_qa_files = {date: glob.glob('/exposures/nightwatch/{}/*/qa-*.fits'.format(date)) for date in self.dates}
-		
-		dfs = []
-		for night, files in all_qa_files.items():
-		    for f in files:
-		        dfs.append(self.get_qa_data(f))
+        all_qa_files = {date: glob.glob('/exposures/nightwatch/{}/*/qa-*.fits'.format(date)) for date in self.dates}
+        
+        dfs = []
+        for night, files in all_qa_files.items():
+            for f in files:
+                dfs.append(self.get_qa_data(f))
 
-		self.qa_df = pd.concat(dfs)
+        self.qa_df = pd.concat(dfs)
 
     def run(self):
-    	self.get_exp_df()
-    	self.get_shack_df()
-    	self.get_spec_df()
-    	spec_cols = ['nir_camera_temp', 'nir_camera_humidity','red_camera_temp', 'red_camera_humidity', 'blue_camera_temp','blue_camera_humidity',
-		'bench_cryo_temp', 'bench_nir_temp','bench_coll_temp', 'ieb_temp']
-		self.spec_df_final = self.combine_specs(self.spec_df_final, spec_cols)
+        self.get_exp_df()
+        self.get_shack_df()
+        self.get_spec_df()
+        spec_cols = ['nir_camera_temp', 'nir_camera_humidity','red_camera_temp', 'red_camera_humidity', 'blue_camera_temp','blue_camera_humidity',
+        'bench_cryo_temp', 'bench_nir_temp','bench_coll_temp', 'ieb_temp']
+        self.spec_df_final = self.combine_specs(self.spec_df_final, spec_cols)
 
-		self.get_gfa_df()
-		gfa_cols = ['ccdtemp','hotpeltier','coldpeltier','filter','humid2','humid3','fpga','camerahumid','cameratemp']
-		self.gfa_df_final = self.combine_specs(self.gfa_df_final, gfa_cols)
+        self.get_gfa_df()
+        gfa_cols = ['ccdtemp','hotpeltier','coldpeltier','filter','humid2','humid3','fpga','camerahumid','cameratemp']
+        self.gfa_df_final = self.combine_specs(self.gfa_df_final, gfa_cols)
 
-		self.get_qa_df()
+        self.get_qa_df()
 
-		all_dfs = [self.exp_df_new, self.gfa_df_final, self.shack_df_final, self.spec_df_final, self.qa_df] 
+        all_dfs = [self.exp_df_new, self.gfa_df_final, self.shack_df_final, self.spec_df_final, self.qa_df] 
         for i, df in enumerate(all_dfs):
             df.reset_index(drop=True, inplace=True)
             all_dfs[i] = df
