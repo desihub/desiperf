0f9bf54cbb85e0b94cadec779e0e7ae244da2bbc
parfa30@gmail.com <desiobserver@desi-4.kpno.noao.edu>
2020-09-22 15:41:41 -0700

    per fiber data and script

--- py/desiperf/instperfapp/data_mgt/get_pos_data.py
+++ py/desiperf/instperfapp/data_mgt/get_pos_data.py
@@ -14,6 +14,7 @@ class POSData():
     def __init__(self, start, end):
         self.start_date = start
         self.end_date = end
+        self.save_dir = './data/per_fiber/'
 
         self.conn = psycopg2.connect(host="desi-db", port="5442", database="desi_dev", user="desi_reader", password="reader")
 
@@ -21,7 +22,7 @@ class POSData():
 
         self.petal_loc_to_id = {0:'4',1:'5',2:'6',3:'3',4:'8',5:'10',6:'11',7:'2',8:'7',9:'9'}
 
-        self.fiberpos = pd.read_csv('./instperfapp//data/fiberpos.csv')
+        self.fiberpos = pd.read_csv('./data/fiberpos.csv')
 
     def get_exp_df(self):
         exp_cols = ['id','data_location','targtra','targtdec','skyra','skydec','deltara','deltadec','reqtime','exptime','flavor','program','lead','focus','airmass',
@@ -37,10 +38,10 @@ class POSData():
 
     def get_fiberpos_data(self, fib):
         init_df = self.fiberpos[self.fiberpos.FIBER == fib]
-        self.ptl_loc = int(np.unique(init_df.PETAL_LOC))
+        self.ptl_loc = int(np.unique(init_df.PETAL))
         self.ptl = self.petal_loc_to_id[self.ptl_loc]
-        self.dev = int(np.unique(init_df.DEVICE_LOC))
-        self.pos_df = pd.merge(self.coord_df, init_df, how='left',left_on=['PETAL_LOC','DEVICE_LOC'], right_on=['PETAL','DEVICE'])
+        self.dev = int(np.unique(init_df.DEVICE))
+        self.pos_df = pd.merge(self.coord_df, init_df, how='inner',left_on=['PETAL_LOC','DEVICE_LOC'], right_on=['PETAL','DEVICE'])
 
 
     def add_posmove_telemetry(self, fib):
@@ -51,8 +52,9 @@ class POSData():
             ix = np.argmin(np.abs(df['time_recorded'] - time))
             idx.append(ix)
         df = df.iloc[idx]
+        df.reset_index(inplace=True, drop=True)
+        self.exp_df_base.reset_index(inplace=True, drop=True)
         self.pos_telem = pd.concat([self.exp_df_base, df], axis=1)
-
 
     def get_coord_data(self):
         nights = np.unique(self.exp_df_new['night'])
@@ -65,17 +67,20 @@ class POSData():
             for f in coord_files:
                 exposure = int(os.path.splitext(os.path.split(f)[0])[0][-6:])
                 try:
+                    
                     df = Table.read(f, format='fits').to_pandas()
                     good = df['OFFSET_0']
                     final_move = np.sort(fnmatch.filter(df.columns, 'OFFSET_*'))[-1]
-                    df = df[['PETAL_LOC', 'DEVICE_LOC','TARGET_RA', 'TARGET_DEC','FIBERASSIGN_X', 'FIBERASSIGN_Y','OFFSET_0',final_move,'FIBER']]
-                    df = df.rename(columns={final_move:'OFFSET_FINAL'})
-                    df['EXPID'] = exposure
-                    self.coord_df(df)
+                    if int(final_move[-1]) > 0:
+                        df = df[['PETAL_LOC', 'DEVICE_LOC','TARGET_RA', 'TARGET_DEC','FIBERASSIGN_X', 'FIBERASSIGN_Y','OFFSET_0',final_move]]
+                        df = df.rename(columns={final_move:'OFFSET_FINAL'})
+                        df['EXPID'] = exposure
+                        self.coord_df.append(df)
+                    else:
+                        print(f, final_move)
                 except:
                     pass
         self.coord_df = pd.concat(self.coord_df)
-
 
     def run(self):
         print('Start: {}'.format(datetime.now()))
@@ -88,5 +93,5 @@ class POSData():
             self.add_posmove_telemetry(fib)
             self.final_fib_df = pd.merge(self.pos_df, self.pos_telem, on=['EXPID'], how='left')
             print('Fib {} Done: {}'.format(fib, datetime.now()))
-            self.final_fib_df.to_csv('{}.csv'.format(fib), index=False) 
+            self.final_fib_df.to_csv(self.save_dir+'{}.csv'.format(fib), index=False) 
 
