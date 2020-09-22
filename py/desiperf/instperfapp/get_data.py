64d83a3355b6c64a079220cf260f3a5af6a6a155
parfa30@gmail.com <desiobserver@desi-4.kpno.noao.edu>
2020-09-22 15:49:23 -0700

    updated with telemetry

--- py/desiperf/instperfapp/get_data.py
+++ py/desiperf/instperfapp/get_data.py
@@ -1,14 +1,17 @@
 
 from data_mgt.get_fp_data import FPData 
 from data_mgt.get_spec_data import SPECData
+from data_mgt.get_pos_data import POSData
 import argparse
 
 
 start_date = '20200121'
 end_date = '20200317'
 
-fpd = FPData(start_date, end_date)
-fpd.run()
+#fpd = FPData(start_date, end_date)
+#fpd.run()
 #specd = SPECData(start_date, end_date)
 #specd.run()
+pos = POSData(start_date, end_date)
+pos.run()
 
